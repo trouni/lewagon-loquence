b2d3da206de00a93e9f559d1732af26ecba04e29
trouni <34345789+trouni@users.noreply.github.com>
2019-06-03 19:43:59 +0900

    Update 20190603073449_create_orders.rb

--- db/migrate/20190603073449_create_orders.rb
+++ db/migrate/20190603073449_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.datetime :date_time
-      t.string :shipping_address
+      t.datetime :purchase_date
+      t.string :shipping_country
       t.integer :order_total
       t.string :external_source
 
