eaa36edd5d64201839457203aea9ef9193ea3ca4
Saad Amrani Joutey <amrani.saad@gmail.com>
2019-06-03 17:55:52 +0900

    with devise check

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_081557) do
+ActiveRecord::Schema.define(version: 2019_06_03_081842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "amazon_orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "buyers", force: :cascade do |t|
     t.string "username"
@@ -64,6 +69,11 @@ ActiveRecord::Schema.define(version: 2019_06_03_081557) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "woocommerce_orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "order_items", "orders"
