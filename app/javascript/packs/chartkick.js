a5b8a2f8860c4c19c8b275e23a203831fcd42d3b
Trouni Tiet <trouni@gmail.com>
2019-06-04 12:52:25 +0900

    fixed chartkick loading

---
+++ app/javascript/packs/chartkick.js
@@ -0,0 +1,21 @@
+// Chartkick
+require("chartkick")
+require("chart.js")
+
+// Highcharts
+//= require highcharts
+//= require highcharts/highcharts-more
+
+// Highcharts Themes
+//= require highcharts/themes/dark-blue
+
+// Highcharts Modules
+//= require highcharts/modules/annotations
+//= require highcharts/modules/data
+//= require highcharts/modules/drilldown
+//= require highcharts/modules/exporting
+//= require highcharts/modules/funnel
+//= require highcharts/modules/heatmap
+//= require highcharts/modules/no-data-to-display
+//= require highcharts/modules/offline-exporting
+console.log("chartkick loaded")
