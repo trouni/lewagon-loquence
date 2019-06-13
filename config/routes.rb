58990872dd85459c0f736c0f065622e975c1ae79
Trouni Tiet <trouni@gmail.com>
2019-06-14 00:57:50 +0900

    fixed conflict for rebasing

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get 'settings/account', to: 'settings#account'
   get 'settings/users', to: 'settings#users'
   post 'settings/invite', to: 'settings#create'
+  patch 'settings/update', to: 'settings#update'
 
   resources :onboarding
   get 'info', to: 'pages#info'
