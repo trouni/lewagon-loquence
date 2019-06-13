8ba676dbfceda966629f8d4f542c0ed5bc31681d
Saad Amrani Joutey <amrani.saad@gmail.com>
2019-06-13 18:44:05 +0900

    static pages (error) and after sign up + sign up process updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_030220) do
+ActiveRecord::Schema.define(version: 2019_06_13_035421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_030220) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "owner_id"
+    t.string "photo"
     t.index ["owner_id"], name: "index_companies_on_owner_id"
   end
 
@@ -157,6 +158,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_030220) do
     t.bigint "company_id"
     t.string "team"
     t.boolean "admin", default: false, null: false
+    t.string "photo"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
