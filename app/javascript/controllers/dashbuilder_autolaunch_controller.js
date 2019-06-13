21d86a4d4c6592a65a40f29917b42d53f096f1fc
Trouni Tiet <trouni@gmail.com>
2019-06-13 13:03:46 +0900

    created widget filters partial

--- app/javascript/controllers/dashbuilder_autolaunch_controller.js
+++ app/javascript/controllers/dashbuilder_autolaunch_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
 
   connect() {
-    document.getElementById('menu-item-edit-report').click();
+    document.getElementById('edit-report-link').click();
   }
 }
